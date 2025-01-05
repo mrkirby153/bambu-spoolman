3c0d0f4915145ed71d7e669cf35349476ff19431
mrkirby153 <mr.austinwhyte@gmail.com>
2025-01-05 14:46:40 -0800

    Format printer status

--- frontend/src/components/PrinterStatus.tsx
+++ frontend/src/components/PrinterStatus.tsx
@@ -1,9 +1,16 @@
+import type { PrinterStatus } from "@app/types";
 import { useSuspenseQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 
+type PrinterStatusData = {
+  connected: boolean;
+  status: PrinterStatus;
+  last_update: number;
+};
+
 export default function PrinterStatus() {
   const queryClient = useQueryClient();
-  const { data } = useSuspenseQuery({
+  const { data } = useSuspenseQuery<PrinterStatusData>({
     queryKey: ["printer-status"],
     queryFn: async () => {
       const response = await fetch("/api/printer-info");
@@ -21,8 +28,22 @@ export default function PrinterStatus() {
     <>
       <h2 className="text-2xl">Printer Status</h2>
       <React.Suspense fallback={<div>Loading...</div>}>
-        <button onClick={refresh}>Refresh</button>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <button onClick={refresh} className="text-blue-500 hover:underline">
+          Refresh
+        </button>
+        <div>
+          Updated: {new Date(data?.last_update * 1000).toLocaleString()}
+        </div>
+        <ul>
+          <li>Connected: {data?.connected ? "Yes" : "No"}</li>
+          <li>Printing: {data?.status?.print?.gcode_file || "Nothing"}</li>
+          {data?.status?.print?.gcode_state == "RUNNING" && (
+            <li>
+              Layer: {data?.status?.print?.layer_num} /{" "}
+              {data?.status?.print?.total_layer_num}
+            </li>
+          )}
+        </ul>
       </React.Suspense>
     </>
   );
