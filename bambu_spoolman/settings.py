fced8db4fcb0e32a534ae10f7b1c932b646868d1
mrkirby153 <mr.austinwhyte@gmail.com>
2025-01-08 18:22:48 -0800

    Support changing the configuration directory

--- bambu_spoolman/settings.py
+++ bambu_spoolman/settings.py
@@ -2,13 +2,21 @@ import json
 import os
 
 
+def _settings_file():
+    configuration_directory = os.environ.get("BAMBU_SPOOLMAN_CONFIG")
+    if configuration_directory is None:
+        return "settings.json"
+    return os.path.join(configuration_directory, "settings.json")
+
+
 def save_settings(settings):
-    with open("settings.json", "w") as f:
+    with open(_settings_file(), "w") as f:
         json.dump(settings, f)
 
 
 def load_settings():
-    if os.path.exists("settings.json"):
-        with open("settings.json") as f:
+    settings_file_path = _settings_file()
+    if os.path.exists(settings_file_path):
+        with open(settings_file_path) as f:
             return json.load(f)
     return {}
